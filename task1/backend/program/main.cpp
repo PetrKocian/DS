020bc79e7b546634ee9aaa1d3e4306fd25323d6e
Petr Kocian <kocian@kth.se>
2022-10-23 22:45:46 +0200

    slave master communication, multithreaded

--- task1/backend/program/main.cpp
+++ task1/backend/program/main.cpp
@@ -12,6 +12,8 @@
 #include <unistd.h>
 #include <vector>
 #include <thread>
+#include <mutex>
+#include <algorithm>
 
 #define PORT 1234
 #define BROADCAST "255.255.255.255"
@@ -21,11 +23,30 @@
 int random_nr = 0;
 bool master = false;
 bool slave = false;
-std::vector<struct sockaddr_in> nodes = {};
+int reds = 0;
+int greens = 0;
+struct sockaddr_in receiveSockaddr;
+socklen_t receiveSockaddrLen = sizeof(receiveSockaddr);
+long socketfd_global;
+enum colors
+{
+  red = 0,
+  green = 1
+};
+std::mutex node_vector_mutex;
+std::mutex int_vector_mutex;
+std::mutex socket_mutex;
+struct node
+{
+  colors color;
+  int node_nr;
+  struct sockaddr_in node_addr;
+};
+std::vector<node> nodes = {};
+std::vector<int> nodes_int = {};
 
 void client(int random_nr)
 {
-  std::cout << "running client" << std::endl;
   char reply[1024];
   int socketfd;
   std::string addr = BROADCAST;
@@ -50,26 +71,139 @@ void client(int random_nr)
   servaddr.sin_family = AF_INET;
   servaddr.sin_port = htons(PORT);
   servaddr.sin_addr.s_addr = iaddr;
-  while (master == false && slave == false)
+
+  while (true)
   {
-
-    sendto(socketfd, msg.c_str(), msg.length(), 0, (sockaddr *)&servaddr, len);
-
-    ssize_t result = recvfrom(socketfd, reply, 1024, 0, (struct sockaddr *)&servaddr, &len);
-
-
-
-    if (result > 0)
+    // send broadcast, wait 60 seconds for reply and then repeat
+    while (master == false && slave == false)
     {
-      std::cout << "becoming slave, reply to broadcast: " << reply << std::endl;
-      slave = true;
-    }
-  }
-  msg = "REPLY TO MASTER" + std::to_string(random_nr) + '\0';
+
       sendto(socketfd, msg.c_str(), msg.length(), 0, (sockaddr *)&servaddr, len);
 
+      ssize_t result = recvfrom(socketfd, reply, 1024, 0, (struct sockaddr *)&servaddr, &len);
+
+      if (result > 0)
+      {
+        std::cout << "becoming slave, received: " << reply << std::endl;
+        slave = true;
+      }
+    }
+
+    // send ping periodically and check for color from master
+    while (slave)
+    { /*
+       char buffer[INET_ADDRSTRLEN];
+       inet_ntop(AF_INET, &servaddr.sin_addr, buffer, sizeof(buffer));*/
+      msg = "PING:" + std::to_string(random_nr) + '\0';
+
+      std::cout << "sending ping to master : " << msg << std::endl;
+      sendto(socketfd, msg.c_str(), msg.length(), 0, (sockaddr *)&servaddr, len);
+      ssize_t result = recvfrom(socketfd, reply, 1024, 0, (struct sockaddr *)&servaddr, &len);
+      if (result < 0)
+      {
+        // master didnt reply, slave is free
+        slave = false;
+      }
+      std::cout << "received from master: " << reply << std::endl;
+      // sleep before next ping
+      usleep(10000000);
+    }
+    usleep(100000000);
+  }
 }
 
+void watchdog(node slave)
+{
+  long socketfd = socketfd_global;
+  std::string msg = "orange";
+  slave.color = red;
+  socket_mutex.lock();
+  sendto(socketfd, msg.c_str(), msg.length(), 0, (struct sockaddr *)&slave.node_addr, receiveSockaddrLen);
+  socket_mutex.unlock();
+  // add node to vector and increment color count
+  node_vector_mutex.lock();
+  if (slave.color == red)
+  {
+    reds++;
+  }
+  else if (slave.color == green)
+  {
+    greens++;
+  }
+  nodes.push_back(slave);
+  node_vector_mutex.unlock();
+
+  // loop to check if node is alive
+  bool alive = true;
+  while (alive)
+  {
+    usleep(30000000);
+    // check if node has pinged since last time
+    int_vector_mutex.lock();
+    auto it = std::find(nodes_int.begin(), nodes_int.end(), slave.node_nr);
+    if (it != nodes_int.end())
+    {
+      std::cout << "node: " << slave.node_nr << " alive" << std::endl;
+      // node alive - erase from ping vector
+
+      socket_mutex.lock();
+      sendto(socketfd, msg.c_str(), msg.length(), 0, (struct sockaddr *)&slave.node_addr, receiveSockaddrLen);
+      socket_mutex.unlock();
+      nodes_int.erase(it);
+    }
+    else
+    {
+      // node dead
+      alive = false;
+    }
+    int_vector_mutex.unlock();
+  }
+  std::cout << "node: " << slave.node_nr << " dead" << std::endl;
+
+  // node dead, remove from vector
+  node_vector_mutex.lock();
+  int i = 0;
+  bool found = false;
+  for (i = 0; i < nodes.size(); i++)
+  {
+    if (nodes.at(i).node_nr == slave.node_nr)
+    {
+      break;
+      found = true;
+    }
+  }
+  if (found)
+  {
+    if (slave.color == red && reds > 0)
+    {
+      reds--;
+    }
+    else if (slave.color == green & greens > 0)
+    {
+      greens--;
+    }
+    nodes.erase(nodes.begin() + i);
+    if (nodes.empty())
+    {
+      master = false;
+    }
+  }
+  node_vector_mutex.unlock();
+}
+/*
+void node_com(long socketfd)
+{
+  char reply[1024];
+
+  char buffer[INET_ADDRSTRLEN];
+  inet_ntop(AF_INET, &receiveSockaddr.sin_addr, buffer, sizeof(buffer));
+  std::cout << "sending hello3 to addr: " << buffer << std::endl;
+  sendto(socketfd, "HELLO3", strlen("HELLO3"), 0, (struct sockaddr *)&receiveSockaddr, receiveSockaddrLen);
+
+  recvfrom(socketfd, reply, 1024, 0, (struct sockaddr *)&receiveSockaddr, &receiveSockaddrLen);
+  std::cout << "reply after hello3: " << reply << std::endl;
+}
+*/
 /*
 void announceNumber(int random_nr)
 {
@@ -144,12 +278,13 @@ void process_reply(std::string reply_in, sockaddr_in address, long socketin)
   servaddr.sin_port = htons(PORT);
   servaddr.sin_addr.s_addr = address.sin_addr.s_addr;
 
-  //std::cout << "addr " << buffer << std::endl;
+  // std::cout << "addr " << buffer << std::endl;
   std::string msg = "REPLY TO ELECTION " + std::to_string(random_nr) + '\0';
   int socketfd;
   sendto(socketin, msg.c_str(), msg.length(), 0, (sockaddr *)&servaddr, len);
   recvfrom(socketin, reply, 1024, 0, (struct sockaddr *)&servaddr, &len);
-  while(1);
+  while (1)
+    ;
   if ((socketfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
   {
     std::cout << "Socket error: " << strerror(errno) << "  " << errno << std::endl;
@@ -171,13 +306,12 @@ void process_reply(std::string reply_in, sockaddr_in address, long socketin)
 
 void server()
 {
-  std::cout << "running server" << std::endl;
   long socketfd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  socketfd_global = socketfd;
   struct sockaddr_in sockaddr;
   memset(&sockaddr, 0, sizeof(sockaddr));
   char reply[1024];
-  struct sockaddr_in receiveSockaddr;
-  socklen_t receiveSockaddrLen = sizeof(receiveSockaddr);
+  std::string msg;
 
   sockaddr.sin_family = AF_INET;
   sockaddr.sin_port = htons(PORT);
@@ -192,24 +326,54 @@ void server()
 
   while (slave == false)
   {
+    std::cout << "listening" << std::endl;
     ssize_t result = recvfrom(socketfd, reply, 1024, 0, (struct sockaddr *)&receiveSockaddr, &receiveSockaddrLen);
-    if(result < 0)
+    if (result < 0)
     {
       continue;
     }
-    std::cout << "received broadcast: " << reply << std::endl;
+    std::cout << "received message: " << reply << std::endl;
     std::string str = reply;
+    memset(&reply, 0, sizeof(reply));
+
     std::size_t pos = str.find("ELECTION:");
-    std::string number_s = str.substr(pos + 9);
-    int number_i = std::stoi(number_s);
-    if (number_i != random_nr && slave == false)
+    if (pos != std::string::npos)
     {
-      std::cout << "becoming master" << std::endl;
-      master = true;
-      sendto(socketfd, "HELLO3", strlen("HELLO3"), 0, (struct sockaddr *)&receiveSockaddr, receiveSockaddrLen);
-      recvfrom(socketfd, reply, 1024, 0, (struct sockaddr *)&receiveSockaddr, &receiveSockaddrLen);
-      std::cout << "reply after hello3: "<< reply << std::endl;
-      //std::thread t(process_reply, reply, receiveSockaddr, socketfd);
+      // first contact with master
+      std::string number_s = str.substr(pos + 9);
+      int number_i = std::stoi(number_s);
+      if (number_i != random_nr && slave == false)
+      {
+        master = true;
+        msg = "WELCOME:" + std::to_string(random_nr) + '\0';
+
+        socket_mutex.lock();
+        sendto(socketfd, msg.c_str(), msg.length(), 0, (struct sockaddr *)&receiveSockaddr, receiveSockaddrLen);
+        socket_mutex.unlock();
+        
+        node slave;
+        slave.node_addr = receiveSockaddr;
+        slave.node_nr = number_i;
+        std::thread t = std::thread(watchdog, slave);
+        t.detach();
+      }
+
+      // continue to listen
+      continue;
+    }
+    pos = str.find("PING:");
+    if (pos != std::string::npos)
+    {
+      // ping
+      std::string number_s = str.substr(pos + 5);
+      int number_i = std::stoi(number_s);
+
+      int_vector_mutex.lock();
+      nodes_int.push_back(number_i);
+      int_vector_mutex.unlock();
+
+      // continue to listen
+      continue;
     }
   }
 }
